2acdcb3e08ab86da1841c1bc58409ad0d339a074
naginata <nikita05062001@mail.ru>
2024-01-09 14:33:49 +0600

    update1

--- src/components/MenuSelect/MenuSelect.jsx
+++ src/components/MenuSelect/MenuSelect.jsx
@@ -10,9 +10,9 @@ const MenuSelect = ({ stateMenu }) => {
       <div className="menu-content">
         <div className="menu-content-list">
           <ul>
-            <li onClick={() => handleItemClick("1")}>Список команд</li>
+            <li onClick={() => handleItemClick("1")}>Команды</li>
             <li onClick={() => handleItemClick("2")}>Авторизация</li>
-            <li onClick={() => handleItemClick("3")}>О компании</li>
+            <li onClick={() => handleItemClick("3")}>О нас</li>
           </ul>
         </div>
       </div>
