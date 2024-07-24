4325eac501afb8f3658330e6b749f9af095295b0
Naginata228 <70873747+nikita05062001@users.noreply.github.com>
2024-07-24 21:47:15 +0500

    24.07.2024 v1

--- src/components/ComandList/ComandList.jsx
+++ src/components/ComandList/ComandList.jsx
@@ -9,6 +9,14 @@ const ComandList = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [selectedElement, setSelectedElement] = useState(null);
+  const [filters, setFilters] = useState({
+    search: "",
+    cat1: "",
+    cat2: "",
+    cat3: ""
+  });
+  const [filteredData, setFilteredData] = useState([]);
+  const categories = {}
 
   useEffect(() => {
     const fetchList = async () => {
@@ -31,6 +39,49 @@ const ComandList = () => {
     fetchList();
   }, []);
 
+
+  useEffect(() => {
+    if (!loading) {
+      list.forEach((el) => {
+        if (!categories[el["Категория 1"]]) {
+          categories[el["Категория 1"]] = {};
+        }
+  
+        if (!categories[el["Категория 1"]][el["Категория 2"]]) {
+          categories[el["Категория 1"]][el["Категория 2"]] = [];
+        }
+  
+        // Добавляем значение, если оно не пустое
+        if (el["Категория 3"].trim() !== "") {
+          categories[el["Категория 1"]][el["Категория 2"]].push(el["Категория 3"]);
+        }
+      });
+  
+      // Удаляем пустые строки и дубликаты из массивов
+      Object.keys(categories).forEach(category1Key => {
+        Object.keys(categories[category1Key]).forEach(category2Key => {
+          categories[category1Key][category2Key] = Array.from(new Set(categories[category1Key][category2Key]))
+            .filter(item => item.trim() !== "");
+        });
+      });
+  
+      console.log(categories);
+    }
+  }, [loading, list]);
+
+  useEffect(() => {
+    if (!loading) {
+      const newFilteredData = list.filter(item => {
+        const searchMatch = item?.Наименование.toLowerCase().includes(filters.search.toLowerCase());
+        const cat1Match = filters.cat1 ? item["Категория 1"] === filters.cat1 : true;
+        const cat2Match = filters.cat2 ? item["Категория 2"] === filters.cat2 : true;
+        const cat3Match = filters.cat3 ? item["Категория 3"] === filters.cat3 : true; // Возможно, ошибка в вашем коде: здесь должно быть category3
+        return searchMatch && cat1Match && cat2Match && cat3Match;
+      });
+      setFilteredData(newFilteredData);
+    }
+  }, [loading, filters, list]);
+
   const handleTitleClick = (el) => {
     setSelectedElement(el);
   };
@@ -38,7 +89,11 @@ const ComandList = () => {
   return (
     <div className="list">
       <div className="list-input">
-        <input type="text" />
+        <input type="text"  value={filters.search} onChange={(e) =>{
+          const newObj = {...filters, search: e.target.value};
+          setFilters(newObj);
+        }} />
+        {/* <img src="https://i.imgur.com/j10HQx7.jpg" alt="test" /> */}
       </div>
       <div className="list-content">
         <ul>
@@ -47,7 +102,7 @@ const ComandList = () => {
             <div className="list-content-count">Кол-во</div>
           </li>
           {!loading ? (
-            list.map((el, index) => (
+            filteredData.map((el, index) => ( // Использование filteredData вместо list
               <li key={index}>
                 <div
                   className="list-content-title"
